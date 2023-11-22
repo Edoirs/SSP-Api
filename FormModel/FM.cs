014698c8429c02d2493e2b6a0a54e476ba9c2556
MideAmos <95050746+mideamos@users.noreply.github.com>
2023-11-22 16:11:11 +0100

    did alot

--- FormModel/FM.cs
+++ FormModel/FM.cs
@@ -11,6 +11,25 @@ namespace SelfPortalAPi
         public string? MDAServiceName { get; set; } = null;
         public string? MDAName { get; set; } = null;
     }
+
+    public class ProjectionFm
+    {
+        public string annual_projection_id { get; set; }
+        public int corporate_id { get; set; }
+        public int app_id { get; set; }
+        public string company_name { get; set; }
+        public string taxpayer_id { get; set; }
+        public int projection_year { get; set; }
+        public string file_projection_status { get; set; }
+        public string forwarded_to { get; set; }
+        public string date_forwarded { get; set; }
+        public string created_at { get; set; }
+        public int employees_count { get; set; }
+        public string business_id { get; set; }
+        public string business_name { get; set; }
+        public int business_primary_id { get; set; }
+        public bool status { get; set; } = false;
+    }
     public class IndividualFormModel
     {
         public string? IndividualRin { get; set; }
---
+++ Mapper/AutoMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SelfPortalAPi.FormModel;
+using SelfPortalAPi.NewTables;
+
+namespace SelfPortalAPi.Mapper
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<employee, AddEmployee>();
+            CreateMap<employee, EmployeeVm>();
+            CreateMap<Projection, ProjectionFm>();
+        }
+    }
+}
